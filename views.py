fc087491cd36323736df1c7c0a82fe0f27110cd0
MuthuRishwin <146799114+MuthuRishwin@users.noreply.github.com>
2024-04-10 18:40:32 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def no_access(request):
-    template = loader.get_template('no_access.html')
+def not_found(request):
+    template = loader.get_template('404.html')
+    return HttpResponse(template.render())
+
+def not_found_404(request,exception):
+    template = loader.get_template('404.html')
     return HttpResponse(template.render())
